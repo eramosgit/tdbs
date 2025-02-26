bbd9624df0fb10cc2784edb81c237044365bc252
eramosgit <intruds@gmail.com>
2025-02-26 22:30:06 +0800

    Wrapp App in HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CssBaseline } from "@mui/material";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
