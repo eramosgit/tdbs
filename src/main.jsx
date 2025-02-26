1d127c8494f3f106e7b6ae1f7730e2b3eaa1da56
eramosgit <intruds@gmail.com>
2025-02-26 22:41:58 +0800

    Removed HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CssBaseline } from "@mui/material";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
